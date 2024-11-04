460b1515643a97c4578fb8b48f35de7bd83c765a
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2024-11-04 14:55:46 -0300

    Created a basic interface for creating the INI config file to store the database credentials and information

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import configparser
 
 PG = {
     "host": os.getenv("POSTGRES_HOST"),
@@ -9,6 +10,34 @@ PG = {
 }
 
 print(PG)
+
+class AttrDict(dict):
+    def __init__(self,*args, **kwargs):
+        super(AttrDict,self).__init__(*args,**kwargs)
+        self.__dict__ = self
+
+def check_ini_config_exists():
+    if os.path.exists('./website/settings/conf.ini'):
+        return True
+    else:
+        return False
+
+def create_ini_config(dbhost:str,dbport:str,dbuser:str,dbpassword:str,db:str):
+    config = configparser.ConfigParser()
+    config.add_section("Database")
+    config.set('Database','Host',dbhost)
+    config.set('Database','Port',dbport)
+    config.set('Database','User',dbuser)
+    config.set('Database','Password',dbpassword)
+    config.set('Database','Database',db)
+    with open('./website/settings/conf.ini','w') as file:
+        config.write(file)
+
+def get_ini_config():
+    config = configparser.ConfigParser(dict_type=AttrDict)
+    config.read('./website/settings/conf.ini')
+    data = AttrDict(config._sections)
+    return data
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = (
